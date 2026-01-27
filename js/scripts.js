5c731562601262ab3b2b129df609dfe5125da0ed
mtoledo2 <murilo_toledo@hotmail.com>
2026-01-27 16:49:15 -0300

    v 1.09

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-/*document.addEventListener('DOMContentLoaded',() => {
-    fetch('./components/menu.html')
+document.addEventListener('DOMContentLoaded',() => {
+    fetch('components/menu.html')
         .then(res => res.text())
         .then(html => {
             document.getElementById('menu-container').innerHTML = html
@@ -14,36 +14,7 @@
                 conteudo.classList.toggle('toggle')    
             })
         })
-})*/
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    const isRoot =
-        location.pathname === '/' ||
-        location.pathname.endsWith('/index.html')
-
-    const menuPath = isRoot
-        ? './components/menu.html'
-        : '../components/menu.html'
-
-    fetch(menuPath)
-        .then(res => res.text())
-        .then(html => {
-            document.getElementById('menu-container').innerHTML = html
-
-            const botao = document.querySelector('.bt-menu')
-            const menuLateral = document.querySelector('.menu-left')
-            const conteudo = document.querySelector('.conteudo')
-
-            if (!botao) return
-
-            botao.addEventListener('click', () => {
-                menuLateral.classList.toggle('toggle')
-                botao.classList.toggle('toggle')
-                conteudo.classList.toggle('toggle')
-            })
-        })
-        .catch(err => console.error('Erro ao carregar menu:', err))
 })
+    
 
 
